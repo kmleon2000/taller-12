998c97fcf03c2fcdb0cf645d491e6f272bcf1087
Karen Leon <kmleonp@correo.udistrital.edu.com>
2021-12-14 23:23:24 -0500

    Primer version del taller 12

---
+++ Taller 12.py
@@ -0,0 +1,20 @@
+def simular_semiparabolico(Vo,y):
+    Yo = y
+    Xo = 0
+    t = 0
+    g = 9.8
+
+    coords = []
+
+    while (Yo - (1/2 * g* (t**2))) >= 0:
+        
+        xcoord = round(Xo + Vo * t,2)                 # x(t) = Xo + Vo * t
+        ycoord = round(Yo - (1/2 * g* (t**2)),2)      # y(t) = Yo - 1/2(g * (t**2))
+        coords.append((xcoord,ycoord))
+        t += 0.2
+
+    return coords
+
+data = simular_semiparabolico(100,400)
+
+print(data)
